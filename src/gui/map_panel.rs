101f8f31b36eda8d9daba42bf945b780fac6ed9c
Thierry Treyer <zehkae@gmail.com>
2021-12-10 05:11:57 +0000

    First draft at handling rewinds

--- src/gui/map_panel.rs
+++ src/gui/map_panel.rs
@@ -1,3 +1,5 @@
+use std::collections::LinkedList;
+use std::mem::replace;
 use std::ops::Mul;
 
 use crate::egui_backend::egui;
@@ -33,19 +35,27 @@ impl MapPanel {
             ui.add(egui::Slider::new(&mut self.image_pos.y, 400.0..=500.0));
             ui.add(egui::Slider::new(&mut self.scale, 5.8..=6.2));
 
-            let points = packets
-                .iter()
-                .map(|p| Value::new(p.position_x, p.position_z));
-            let player_track = plot::Line::new(Values::from_values_iter(points));
+            let mut last_distance = f32::NEG_INFINITY;
+            let mut current_line = Vec::new();
+            let mut lines = LinkedList::new();
+            for p in packets.iter() {
+                if p.distance_traveled < replace(&mut last_distance, p.distance_traveled) {
+                    lines.push_back(replace(&mut current_line, Vec::new()));
+                }
+
+                current_line.push(Value::new(p.position_x, p.position_z));
+            }
+            lines.push_back(current_line);
 
             let image_plot =
                 PlotImage::new(self.image, self.image_pos, self.image_size.mul(self.scale));
 
+            let plot = plot::Plot::new("Map").data_aspect(1.0).image(image_plot);
             ui.add(
-                plot::Plot::new("Map")
-                    .data_aspect(1.0)
-                    .image(image_plot)
-                    .line(player_track),
+                lines
+                    .into_iter()
+                    .map(|points| plot::Line::new(Values::from_values(points)))
+                    .fold(plot, |plot, track| plot.line(track)),
             );
         });
     }
