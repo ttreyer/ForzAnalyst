11420c10980da916570c670183377840551db64b
Thierry Treyer <zehkae@gmail.com>
2021-12-25 21:50:09 +0100

    Use defaults NativeOptions

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -4,14 +4,9 @@ fn main() {
     eframe::run_native(
         Box::new(App::default()),
         eframe::NativeOptions {
-            always_on_top: false,
-            decorated: true,
-            icon_data: None,
-            initial_window_size: None,
             maximized: true,
-            drag_and_drop_support: false,
-            resizable: true,
-            transparent: false,
+            drag_and_drop_support: true,
+            ..Default::default()
         },
     )
 }
